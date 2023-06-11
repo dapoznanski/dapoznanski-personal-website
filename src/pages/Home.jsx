acd939b4b48d895a3468067db1a919edd0ee99c5
Diana Apoznanski <dapoznanski@Dianas-MacBook-Pro.local>
2023-06-11 18:30:24 -0400

    update home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+import ImageCarousel from "../components/ImageCarousel";
+const Home = () => {
+    return (
+        <>
+            <ImageCarousel />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
